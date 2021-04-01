b78f51acad35005b91bbe63055671faca1f1f4c7
PepeAndVit <joseph_andvit@hotmail.com>
2021-04-01 13:11:17 +0200

    JWT login, signin and profile authentication

---
+++ dist/controllers/auth.controller.js
@@ -0,0 +1,53 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.profile = exports.signin = exports.signup = void 0;
+const User_1 = __importDefault(require("../models/User"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const signup = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(req.body);
+    const user = new User_1.default({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.password = yield user.encryptPassword(user.password);
+    const savedUser = yield user.save();
+    // Token
+    const token = jsonwebtoken_1.default.sign({ id: savedUser.id }, process.env.TOKEN_SECRET || 'tokentest');
+    res.header('aut-token', token).json(savedUser);
+});
+exports.signup = signup;
+const signin = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(req.body);
+    const user = yield User_1.default.findOne({ email: req.body.email });
+    if (!user)
+        return res.status(400).json('Email or password is wrong');
+    const correctPassword = yield user.validatePassword(req.body.password);
+    if (!correctPassword)
+        return res.status(400).json('Invalid Password');
+    const token = jsonwebtoken_1.default.sign({ id: user.id }, process.env.TOKEN_SECRET || 'tokentest', {
+        expiresIn: 60 * 60 * 24
+    });
+    res.header('auth-token', token).json(user);
+});
+exports.signin = signin;
+const profile = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = yield User_1.default.findById(req.userId, { password: 0 });
+    if (!user)
+        return res.status(404).json('No user found');
+    res.json(user);
+});
+exports.profile = profile;
+//# sourceMappingURL=auth.controller.js.map
